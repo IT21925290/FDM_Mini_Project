27c7cf4f9103186e4f1c61e16b426ceae084b9f3
IT21303098 <it21303098@my.sliit.lk>
2023-10-20 15:20:51 +0530

    change

--- pages/Predict.py
+++ pages/Predict.py
@@ -178,3 +178,15 @@ with col2:
             reverse = False,
             loop = True
         )
+
+        def load_lottiefile(filepath: str):
+            with open(filepath, "r") as f:
+                return json.load(f)
+            
+        lottie_coding = load_lottiefile("C:\\Users\\Admin\\Desktop\\FDM\\FDM_Mini_Project\\animations\\p3.json")
+        st_lottie(
+            lottie_coding,
+            speed =1,
+            reverse = False,
+            loop = True
+        )
